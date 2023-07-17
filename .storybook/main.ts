c2d13a11e1ce7c03e3a829dafb2da3f0eb6ae155
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2023-07-17 23:09:51 +1000

    Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,31 @@
+import type {StorybookConfig} from '@storybook/nextjs';
+
+const path = require('path');
+
+const config: StorybookConfig = {
+    stories: ['../components/**/*.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-styling',
+    ],
+    framework: {
+        name: '@storybook/nextjs',
+        options: {},
+    },
+    docs: {
+        autodocs: 'tag',
+    },
+    webpackFinal: async (config) => {
+        if (config.resolve) {
+            config.resolve.alias = {
+                ...config.resolve.alias,
+                '@': path.resolve(__dirname, '../'),
+            };
+        }
+        return config;
+    },
+};
+
+export default config;
---
+++ components/ui/accordion.stories.tsx
@@ -0,0 +1,34 @@
+import {Meta, StoryObj} from "@storybook/react";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
+
+const meta: Meta<typeof Accordion> = {
+    component: Accordion
+}
+
+export default meta;
+type Story = StoryObj<typeof Accordion>;
+
+export const Primary: Story = {
+    render: () => <Accordion type="single">
+        <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionContent>
+                Yes. It comes with default styles that matches the other
+                components&apos; aesthetic.
+            </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionContent>
+                Yes. It&apos;s animated by default, but you can disable it if you
+                prefer.
+            </AccordionContent>
+        </AccordionItem>
+    </Accordion>
+}
