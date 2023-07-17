132d855832844924e3913d889f3b149b46ba81e2
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2023-07-17 23:27:12 +1000

    Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -14,18 +14,6 @@ const preview: Preview = {
             },
         },
     },
-
-    decorators: [
-        // Adds theme switching support.
-        // NOTE: requires setting "darkMode" to "class" in your tailwind config
-        withThemeByClassName({
-            themes: {
-                light: 'light',
-                dark: 'dark',
-            },
-            defaultTheme: 'light',
-        }),
-    ],
 };
 
 export default preview;
---
+++ components/ui/alert.stories.tsx
@@ -0,0 +1,20 @@
+import {Meta, StoryObj} from "@storybook/react";
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
+import {Terminal} from "lucide-react";
+
+const meta: Meta<typeof Alert> = {
+    component: Alert
+}
+
+export default meta;
+type Story = StoryObj<typeof Alert>;
+
+export const Primary: Story = {
+    render: () => <Alert>
+        <Terminal className="h-4 w-4"/>
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+            You can add components to your app using the cli.
+        </AlertDescription>
+    </Alert>
+}
